a81a45fd5fdba271fca2c566bea3022b723bdc38
Gabriel Eidelman <gabeaeidelman@gmail.com>
2025-05-28 15:09:40 -0700

    setup(main): create main template for starting fastapi server

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
-from app.api import agent_router
+from app.api.fastapi_manager import chat
 
 app = FastAPI()
-app.include_router(agent_router.router)
+app.post("/chat")(chat)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+
+# uvicorn app.main:app --reload
