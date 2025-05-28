2e91e84ec1b6d9398d8eafd968fe02d8fa6e060e
Gabriel Eidelman <gabeaeidelman@gmail.com>
2025-05-28 15:10:24 -0700

    fix(config): fix llm_config api keys

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,33 @@
 import os
 from autogen import LLMConfig
+from pydantic import BaseModel
 
 llm_config = LLMConfig(
     config_list=[
         {
             "api_type": "azure",
             "api_key": os.environ["OPENAI_API_KEY"],
-            "api_version": "version",
-            "base_url": "url",
+            "api_version": "2024-12-01-preview",
+            "base_url": "https://gabriel-azure-oai.openai.azure.com/",
             "model": "gpt-4o-mini",  # Must match the model your deployment refers to
         }
     ],
     temperature=0.7
+)
+
+class ChatSummary(BaseModel):
+    main_topic: str
+    class_schedule: str
+
+summary_llm_config = LLMConfig(
+    config_list=[
+        {
+            "api_type": "azure",
+            "api_key": os.environ["OPENAI_API_KEY"],
+            "api_version": "2024-12-01-preview",
+            "base_url": "https://gabriel-azure-oai.openai.azure.com/",
+            "model": "gpt-4o-mini",  # Must match the model your deployment refers to
+        }
+    ],
+    response_format=ChatSummary
 )
